0b3c70fda5ca6211ea41d8f64bd9f6694c2e2a36
Atharva Ranade <atharvaranade4@gmail.com>
2022-10-31 20:30:31 -0700

    first pass

--- Class 2 Exercises and Homework.js
+++ Class 2 Exercises and Homework.js
@@ -4,22 +4,38 @@
 // - lastName
 // - 'favorite food'
 // - bestFriend (object with the same 3 properties as above)
+const person = {
+  firstName: "Atharva",
+  lastName: "Ranade",
+  'favorite food': "Indian",
+
+  bestFriend: {
+      firstName: "Arjun",
+      lastName: "Kapoor",
+      'favorite food': "Mexican",
+  }
+}
 
 
 // 2. console.log best friend's firstName and your favorite food
-
+console.log(person.bestFriend.firstName);
+console.log(person["favorite food"]);
 
 // 3. Create an array to represent this tic-tac-toe board
 // -O-
 // -XO
 // X-X
+ttt = [['-','0','X'],['-','X','0'],['X','-', 'X']]
+console.table(ttt)
 
 
 // 4. After the array is created, 'O' claims the top right square.
 // Update that value.
+ttt[0][2] = '0'
 
 
 // 5. Log the grid to the console.
+console.table(ttt)
 
 
 // 6. You are given an email as string myEmail, make sure it is in correct email format.
@@ -29,16 +45,25 @@
 // - Use rubular to check a few emails: https://rubular.com/
 // - Use regexp test method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
 
+const myEmail = 'test @guy.com';
+
+const emailRegex = new RegExp('/\w+([.-_]\w+)*@\w+([.-_]\w+)*\.\w{2,4}/g');
+const validEmail = emailRegex.test(myEmail);
+
+console.log(validEmail)
 
 // 7. You are given an assignmentDate as a string in the format "month/day/year"
 // i.e. '1/21/2019' - but this could be any date.
 // Convert this string to a Date
 const assignmentDate = '1/21/2019';
-
+const currentDate = new Date(assignmentDate);
+console.log("current date is", currentDate);
 
 // 8. Create a new Date instance to represent the dueDate.  
 // This will be exactly 7 days after the assignment date.
-
+const dueDate = new Date(currentDate);
+dueDate.setDate(currentDate.getDate() + 7);
+console.log("dueDate is ", dueDate);
 
 // 9. Use dueDate values to create an HTML time tag in format
 // <time datetime="YYYY-MM-DD">Month day, year</time>
@@ -58,5 +83,16 @@ const months = [
   'December'
 ];
 
+//Get year, month, and day from the date
+let year = dueDate.toLocaleString("default", { year: "numeric" });
+let month = dueDate.toLocaleString("default", { month: "2-digit" });
+let day = dueDate.toLocaleString("default", { day: "2-digit" });
+// Generate yyyy-mm-dd date string
+const datetime = year + "-" + month + "-" + day;
+
+console.log(datetime)
+
+document.getElementById("date").innerHTML = datetime;
 
 // 10. log this value using console.log
+
